bf9e0278d8c29f4d1013725249ad7d238ada29ab
FabrizioPerezPeralta <fp2023077476@virtual.upt.pe>
2026-05-02 23:49:13 -0500

    Change API base URL to production endpoint

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5129";
+const API_BASE_URL = "https://back.bdanalyzer.lat";
 
 const parseError = async (response: Response) => {
   const text = await response.text();
